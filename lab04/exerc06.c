8c7b652cf307c477b36297bcfa8c35a36d42600c
Matheus Alefe <matheus.orvati@hotmail.com>
2023-11-15 12:52:36 -0300

    Adicionar exerc06

--- lab04/exerc06.c
+++ lab04/exerc06.c
@@ -9,10 +9,23 @@ void swap(int** a, int** b){
     *b = temp;
 }
 
+void imprime_vetor(int* vet, int n){
+    int i;
+    printf("{");
+    for(i = 0; i < n; i++){
+        printf(" %d", vet[i]);
+    }
+    printf(" }\n");
+}
+
 int main(){
     int a[3] = { 2, 3, 4 }, b[2] = { 5, 6 };
     
     swap(&a, &b);
-    
+    printf("a: ");
+    imprime_vetor(a, 2);
+    printf("b: ");
+    imprime_vetor(b, 3);
+
     return 0;
 }
